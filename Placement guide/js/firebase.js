0e7df2b36a22aee43bf89dba3a20d39d5c20410e
RutwikRK <93569192+RutwikRK@users.noreply.github.com>
2022-05-04 11:28:29 +0530

    Add files via upload

---
+++ Placement guide/js/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    
+    apiKey: "AIzaSyAr5kNnLE0FjUCV0FFNljON4q9mfiCqqVU",
+    authDomain: "pppp-a9cde.firebaseapp.com",
+    databaseURL: "https://pppp-a9cde-default-rtdb.firebaseio.com",
+    projectId: "pppp-a9cde",
+    storageBucket: "pppp-a9cde.appspot.com",
+    messagingSenderId: "808763046401",
+    appId: "1:808763046401:web:0b8266641f51ffdc419e00"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
